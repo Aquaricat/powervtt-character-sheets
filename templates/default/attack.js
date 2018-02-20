781ae40f9ada6b41455e362310a469a55c3696a7
Thomas Lackemann <tommylackemann@gmail.com>
2018-02-20 15:33:22 -0500

    update attack sample

--- templates/default/attack.js
+++ templates/default/attack.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react'
 
 export default class AttackTemplate extends Component {
+  constructor () {
+    super(...arguments)
+
+    this._testMacro = this._testMacro.bind(this)
+  }
+
+  _testMacro () {
+    this.props.executeMacro('!r 1d20 [Woo hoo]')
+  }
+
   render () {
     return (
       <div>
         <h1>From chat template</h1>
         {JSON.stringify(this.props.data)}
+        <a onClick={this._testMacro}>Test Macro</a>
       </div>
     )
   }
